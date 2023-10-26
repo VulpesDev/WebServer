18dda40e95d9b9a1433373380f5ac4a586ea34b6
mcutura <mcutura@student.42berlin.de>
2023-10-26 02:43:35 +0200

    added handling multiple ports per server

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -10,12 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERVER_HPP
-# define SERVER_HPP
+#ifndef WEBSERV_SERVER_HPP
+# define WEBSERV_SERVER_HPP
 
 # include <csignal>
 # include <cstdlib>
 # include <iostream>
+# include <iterator>
+# include <set>
 # include <string>
 
 # include <arpa/inet.h>
@@ -27,24 +29,25 @@
 
 class Server
 {
-
 	public:
 		Server();
 		~Server();
 
+		/* sets and binds sockets for the server on given/default port(s) */
 		bool initialize();
+		/* starts polling loop on listening socket(s) and any connected fds */
 		void start();
 
 	private:
-		static int const	BACKLOG_ = 10;
-		int					listen_fd_;
-		int					epoll_fd_;
-		std::string			port_;
-		bool				isrunning_;
+		static int const		BACKLOG_ = 10;
+		bool					isrunning_;
+		int						epoll_fd_;
+		std::set<std::string>	port_;
+		std::set<int>			listen_fds_;
 
 		static void signal_handler(int signo);
-		bool setup_socket(void);
-		void add_client(void);
+		bool setup_socket(std::string const &port);
+		void add_client(int listen_fd);
 		void close_connection(int fd);
 		void handle_request(int fd);
 
@@ -53,4 +56,4 @@ class Server
 		Server &operator=(Server const & rhs);
 };
 
-#endif  /* SERVER_H */
+#endif  /* WEBSERV_SERVER_HPP */
--- src/Server.cpp
+++ src/Server.cpp
@@ -12,39 +12,45 @@
 
 #include "Server.hpp"
 
-Server::Server() : port_("14242")
-{}
+Server::Server()
+{
+	if (!port_.insert("14242").second)
+		std::cerr << "Failed to store default listening port" << std::endl;
+}
 
 Server::~Server()
 {
-	(void)close(this->listen_fd_);
+	for (std::set<int>::iterator it = this->listen_fds_.begin(); \
+		it != this->listen_fds_.end(); ++it) {
+			(void)close(*it);
+		}
 	(void)close(this->epoll_fd_);
 }
 
 bool Server::initialize()
 {
-	if (!this->setup_socket())
+	if (this->port_.empty())
 		return false;
-	if (listen(this->listen_fd_, this->BACKLOG_) == -1) {
-		std::cerr << "Failed to listen on bound socket" << std::endl;
-		(void)close(this->listen_fd_);
-		return false;
-	}
+	for (std::set<std::string>::iterator it = this->port_.begin(); \
+		it != this->port_.end(); ++it) {
+			if (!this->setup_socket(*it)) {
+				return false;
+			}
+		}
 	if ((this->epoll_fd_ = epoll_create(42)) == -1) {
 		std::cerr << "Failed to create epoll file descriptor" << std::endl;
-		(void)close(this->listen_fd_);
 		return false;
 	}
-
-	epoll_event event;
-	event.events = EPOLLIN;
-	event.data.fd = this->listen_fd_;
-	if (epoll_ctl(this->epoll_fd_, EPOLL_CTL_ADD, this->listen_fd_, &event)) {
-		std::cerr << "Failed to add socket_fd to epoll_ctl" << std::endl;
-		(void)close(this->listen_fd_);
-		(void)close(this->epoll_fd_);
-		return false;
-	}
+	epoll_event event = {};
+	for (std::set<int>::iterator it = this->listen_fds_.begin(); \
+		it != this->listen_fds_.end(); ++it) {
+			event.events = EPOLLIN;
+			event.data.fd = *it;
+			if (epoll_ctl(this->epoll_fd_, EPOLL_CTL_ADD, *it, &event)) {
+				std::cerr << "Failed to add fd to epoll_ctl" << std::endl;
+				return false;
+			}
+		}
 	std::cout << "Server initialized" << std::endl;
 	return true;
 }
@@ -65,12 +71,13 @@ void Server::start()
 			return ;
 		}
 		for (int i = 0; i < n_events; ++i) {
-			if (events[i].data.fd == this->listen_fd_) {
-				this->add_client();
+			int fd = events[i].data.fd;
+			if (this->listen_fds_.find(fd) != this->listen_fds_.end()) {
+				this->add_client(fd);
 			} else if (events[i].events & (EPOLLERR | EPOLLHUP)) {
-				this->close_connection(events[i].data.fd);
+				this->close_connection(fd);
 			} else {
-				this->handle_request(events[i].data.fd);
+				this->handle_request(fd);
 			}
 		}
 	}
@@ -78,31 +85,30 @@ void Server::start()
 
 void Server::signal_handler(int signo)
 {
-	std::cout << signo << " Server shutting down" << std::endl;
+	std::cout << " Server shutting down" << std::endl;
 	std::exit(signo);
 }
 
-bool Server::setup_socket()
+bool Server::setup_socket(std::string const &port)
 {
 	addrinfo	*servinfo;
 	addrinfo	*ptr;
 	addrinfo	hints = {};
+	int			sfd(-1);
 
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	if (int ret = getaddrinfo(NULL, this->port_.c_str(), &hints, &servinfo)) {
+	if (int ret = getaddrinfo(NULL, port.c_str(), &hints, &servinfo)) {
 		std::cerr << "getaddrinfo error: " \
 			<< gai_strerror(ret) << std::endl;
 		return false;
 	}
 	for(ptr = servinfo; ptr != NULL; ptr = ptr->ai_next) {
-		this->listen_fd_ = socket(ptr->ai_family, \
+		sfd = socket(ptr->ai_family, \
 			ptr->ai_socktype | SOCK_NONBLOCK | SOCK_CLOEXEC, ptr->ai_protocol);
-		if (this->listen_fd_ == -1)
-			continue;
-		if (bind(this->listen_fd_, ptr->ai_addr, ptr->ai_addrlen) == -1)
+		if (sfd == -1 || bind(sfd, ptr->ai_addr, ptr->ai_addrlen) == -1)
 			continue;
 		break;
 	}
@@ -112,10 +118,19 @@ bool Server::setup_socket()
 		return false;
 	}
 	freeaddrinfo(servinfo);
+	if (listen(sfd, this->BACKLOG_) == -1) {
+		std::cerr << "Failed to listen on bound socket" << std::endl;
+		(void)close(sfd);
+		return false;
+	}
+	if (!this->listen_fds_.insert(sfd).second) {
+		std::cerr << "Failed to store listening socket" << std::endl;
+		return false;
+	}
 	return true;
 }
 
-void Server::add_client()
+void Server::add_client(int listen_fd)
 {
 	sockaddr_storage	addr;
 	socklen_t			addr_size = sizeof addr;
@@ -123,7 +138,7 @@ void Server::add_client()
 	char 				ipstr[INET6_ADDRSTRLEN];
 	int					port;
 
-	int client_fd = accept(this->listen_fd_, (sockaddr *)&addr, &addr_size);
+	int client_fd = accept(listen_fd, (sockaddr *)&addr, &addr_size);
 	if (client_fd == -1) {
 		std::cerr << "Failed to accept connection" << std::endl;
 		return ;
@@ -138,7 +153,7 @@ void Server::add_client()
 			sockaddr_in *s = (struct sockaddr_in *)&addr;
 			port = ntohs(s->sin_port);
 			inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
-		} else { // AF_INET6
+		} else {
 			struct sockaddr_in6 *s = (struct sockaddr_in6 *)&addr;
 			port = ntohs(s->sin6_port);
 			inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
