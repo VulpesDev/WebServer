aaef5d8d126232b37000c1665b514bb33dd2b284
mcutura <mcutura@student.42berlin.de>
2023-10-29 11:23:09 +0100

    improved test... lil bit

--- tests/client.php
+++ tests/client.php
@@ -3,15 +3,12 @@
 function main(): int
 {
 	$sock = socket_create(AF_INET, SOCK_STREAM, 0);
-	$conn = socket_connect($sock, "127.0.0.1", 14242);
+	$conn = socket_connect($sock, "127.0.0.1", 8080);
 	if (!$conn)
 		return 1;
-	socket_write($sock, "GET /index.html HTTP/1.1\r\n", 26);
+	socket_write($sock, "GET /index.html HTTP/1.1\r\nHost: localhost\r\nUser-Agent: Test Client\r\nConnection: keep-alive\r\n", 84);
 	sleep(1);
-	socket_write($sock, "POST /submit-data HTTP/1.1\r\nHost: intra.42.fr\r\nUser-Agent: My HTTP Client\r\nContent-Type: application/json\r\nContent-Length: 42\r\nConnection: keep-alive\r\n\r\n{\"name\": \"John\", \"age\": 30}\r\n", 182);
-	sleep(1);
-	socket_write($sock, "GET /compressed-data HTTP/1.1\r\nHost: intra.42.fr\r\nUser-Agent: My HTTP Client\r\nAccept-Encoding: gzip, deflate\r\nConnection: close\r\n", 129);
-	sleep(1);
+	socket_write($sock, "GET /compressed-data HTTP/1.1\r\nHost: localhost\r\nUser-Agent: Test Client\r\nAccept-Encoding: gzip, deflate\r\nConnection: close\r\n", 114);
 	socket_close($sock);
 	return 0;
 }
