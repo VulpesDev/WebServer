34adc8fcd5bfd18150435193e2a581588440a85d
Tsvetoslav Vasilev <tsvetivasilev@gmail.com>
2023-11-13 22:29:52 +0100

    Filling data (port bodysize servname)

--- include/ServerConfig.hpp
+++ include/ServerConfig.hpp
@@ -23,6 +23,20 @@
 # define SYMBOLS "{};=,#"
 # define KEYWORDS "http server location"
 
+# define K 1000
+# define M 1000*1000
+# define G 1000*1000*1000
+
+# define MaxPortNum 65535 //Suggested by chatGPT
+
+typedef std::map<std::vector<int>, std::string>	errPages_map;
+typedef std::map<std::vector<int>, std::string>::iterator	errPages_it;
+typedef std::map<std::vector<int>, std::string>::const_iterator	errPages_itc;
+
+typedef std::multimap<std::string, std::vector<std::string>> otherVals_map;
+typedef std::multimap<std::string, std::vector<std::string>>::iterator otherVals_it;
+typedef std::multimap<std::string, std::vector<std::string>>::const_iterator otherVals_itc;
+
 enum TokenType {
 	KEYWORD,
     WORD,
@@ -32,30 +46,32 @@ enum TokenType {
 };
 
 struct Token {
-    TokenType type;
-    std::string value;
+    TokenType		type;
+    std::string		value;
 	unsigned int	line;
 };
 
 struct Location {
-	std::string path;
-	std::string root;
-	std::multimap<std::string, std::vector<std::string>> other_vals;
+	std::string		path;
+	std::string		root;
+	otherVals_map	other_vals;
 };
 
 struct Server {
-	std::string server_name;
-	std::string host;
-	int port;
-	std::vector<Location> locations;
-	std::multimap<std::string, std::vector<std::string>> other_vals;
+	static bool dhp_set;
+	static std::pair<std::string, int>	default_host_port;
+	std::vector<std::string>			server_name;
+	int									max_body_size;
+	int									port;
+	errPages_map						err_pages;
+	otherVals_map						other_vals;
+	std::vector<Location>				locations;
 };
 
 struct Http {
-	std::vector<Server> servers;
-	std::multimap<std::string, std::vector<std::string>> other_vals;
+	std::vector<Server>	servers;
+	otherVals_map		other_vals;
 };
-
 
 static std::string const DEFAULT_CONFIG_PATH = "./data/webserv.default.conf";
 
@@ -72,15 +88,21 @@ class ServerConfig
 		std::vector<Token> tokenize(std::ifstream& file);
 
 	private:
-		bool				is_valid_;
-		bool				parse(std::ifstream& file);
-		bool				isValidBraces(std::vector<Token> tokens);
-		bool				isValidSemicolon(std::vector<Token> tokens);
-		bool				isValidEncapsulation(std::vector<Token> tokens);
 
 		std::vector<Token>	tokens;
 		std::vector<Server>	servers;
 		std::vector<Http>	https;
+
+		bool				is_valid_;
+		bool				parse(std::ifstream& file);
+
+		int					mapToStruct(Http& h);
+		int					mapToStruct(Server& s);
+		int					mapToStruct(Location& l);
+
+		bool				isValidBraces(std::vector<Token> tokens);
+		bool				isValidSemicolon(std::vector<Token> tokens);
+		bool				isValidEncapsulation(std::vector<Token> tokens);
 };
 
 #endif  // WEBSERV_SERVERCONFIG_HPP
--- src/ServerConfig.cpp
+++ src/ServerConfig.cpp
@@ -94,7 +94,9 @@ Server				parseServer(std::vector<Token>::iterator& it, std::vector<Token> token
 				it++;
 			}
 			if (!val_key.empty())
+			{
 				s.other_vals.insert(std::pair<std::string, std::vector<std::string>>(val_key, val_values));
+			}
 		} it++;
 	} return s;
 }
@@ -164,4 +166,108 @@ bool				ServerConfig::parse(std::ifstream& file) {
 		}
 	}
     return true;
+}
+
+int	ServerConfig::mapToStruct(Http& h)
+{
+	//Not required by subject?
+}
+
+int	servName_validate_fill(otherVals_itc it, Server& s)
+{
+	if (it->first == "server_name") {
+		s.server_name = it->second;									//Maybe check if the it->second is not empty
+	}
+	return 0;
+}
+
+int	maxBodySize_validate_fill(otherVals_itc it, Server& s)
+{
+	if (it->first == "client_max_body_size") {
+		std::string	val = it->second.at(0);							//possibly not existing
+		int			numVal = std::atoi(it->second.at(0).c_str());	//maybe return error if this shit is 0
+		char	c = val.back();
+		switch (c) {
+		case K:
+			numVal * K;
+			break;
+		case M:
+			numVal * M;
+			break;
+		case G:
+			numVal * G;
+			break;
+		default:
+			break;
+		}
+		if (numVal == 0) //should return an error
+			;
+		s.max_body_size = numVal;
+	}
+	return 0;
+}
+
+int	host_port_validate_fill(otherVals_itc it, Server& s)
+{
+	int	result = std::atoi(it->second.at(0).c_str()); //return err if this is empty I guess
+	if (result > 0 && result < MaxPortNum)
+		s.port = result;
+	else
+		;//error
+	return 0;
+}
+
+bool fileExists(const std::string& filePath) {
+    std::ifstream file(filePath);
+    return file.good();
+}
+
+bool isNumeric(const std::string& str) {
+    for (std::string::const_iterator it = str.begin(); it != str.end(); ++it) {
+        if (!isdigit(*it)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int	errorPages_validate_fill(errPages_itc it, Server& s)
+{
+	std::string	path;
+	std::vector<int> errors;
+
+	path = it->second.back(); // check if its null
+	if (path.empty() || !fileExists(path))
+		;//error
+	
+	//fill in the errors array
+	// for (size_t i = 0; i < count; i++) {
+	// 	/* code */
+	// }
+	
+}
+
+int	ServerConfig::mapToStruct(Server& s)
+{
+	//if the static bool is not set to true set it
+	if (!s.dhp_set)
+		s.dhp_set = true;
+	for (otherVals_itc it = s.other_vals.begin(); it != s.other_vals.end(); it++) {
+		//set server_name
+		if (servName_validate_fill(it, s)) {
+			//do error
+		}
+		//set max_bodysize
+		if (maxBodySize_validate_fill(it, s)) {
+			//do error
+		}
+		//set host and port
+		if (host_port_validate_fill(it, s)) {
+			//do error
+		}
+		//set error_pages
+		if (errorPages_validate_fill(it, s)) {
+			//do error
+		}
+	}
 }
